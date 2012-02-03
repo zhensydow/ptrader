0cab27026d1321ba3253904de0cbbe4f84253d39
Luis Cabellos <cabellos@ifca.unican.es>
2012-02-03 10:39:06 +0100

    document example01

--- examples/example01.hs
+++ examples/example01.hs
@@ -1,10 +1,41 @@
-import PTrader.Report
-import Control.Monad
-import System.Posix.Unistd
+{- -----------------------------------------------------------------------------
+PTrader is a Personal Stock Trader Toolbox.
+Copyright (C) 2012  Luis Cabellos
 
-main = do
-  let stocks = ["ENG.MC","SAN.MC","TEF.MC","OHL.MC","FER.MC"]
-  
-  forever $ do
-    runReport (newScreen >> stocksState stocks) True
-    sleep 60
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+----------------------------------------------------------------------------- -}
+import PTrader.Report( Report, runReport, newScreen, stocksState )
+import Control.Monad( forever )
+import System.Posix.Unistd( sleep )
+
+-- -----------------------------------------------------------------------------
+stocks :: [String]
+stocks = ["ENG.MC","SAN.MC","TEF.MC","OHL.MC","FER.MC"]
+
+myReport :: Report ()
+myReport = do
+  -- clear console screen
+  newScreen
+  -- print the state of selected stocks
+  stocksState stocks
+
+-- -----------------------------------------------------------------------------
+main :: IO ()
+main = forever $ do
+  -- print myReport using colors
+  runReport myReport True
+  -- wait 1 minute
+  sleep 60
+
+-- -----------------------------------------------------------------------------
